e85b2f7f67df34e6bc50c1d819d4db9953166218
zhengqizhang <770166635@qq.com>
2019-01-11 13:54:31 +0800

    add sudo

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ var bodyParser = require('body-parser');
 
 app.post('/bermi_website', bodyParser.json(), (req, res) => {
     exec(`sudo docker pull ${req.body.repository.repo_name}:${req.body.push_data.tag} &&
-          docker rm $(docker stop bermi_website) &&
-          docker run -d --name bermi_website -p 8085:8085 ${req.body.repository.repo_name}:${req.body.push_data.tag}`
+          sudo docker rm $(sudo docker stop bermi_website) &&
+          sudo docker run -d --name bermi_website -p 8085:8085 ${req.body.repository.repo_name}:${req.body.push_data.tag}`
         ,function (err, out, code) {})
 })
 
-app.get('/test', (req, res) => res.send('Hello World!'))
+app.get('/test', (req, res) => res.send('Hello World!!!'))
 
 app.listen(3000, () => console.log('app listening on port 3000!'))
