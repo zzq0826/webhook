30afb12050072d81ebe86149c91231159c93ef88
zhengqizhang <770166635@qq.com>
2019-02-26 16:11:22 +0800

    change command

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ const express = require('express')
 const app = express()
 var exec = require("shelljs").exec;
 var bodyParser = require('body-parser');
-const delay = process.env.DELAY
-
-console.log(delay)
+// const delay = process.env.DELAY
 
 app.post('/bermi_website', bodyParser.json(), (req, res) => {
-    setTimeout(()=>{
+    // setTimeout(()=>{
       exec(`sudo docker pull ${req.body.repository.repo_name}:${req.body.push_data.tag} &&
-          sudo docker rm $(sudo docker stop bermi_website) &&
+          sudo docker stop bermi_website &&
           sudo docker run --rm -d --name bermi_website -p 8085:8085 ${req.body.repository.repo_name}:${req.body.push_data.tag}`
         ,function (err, out, code) {})
-    }, delay)
+    // }, delay)
 })
 
 app.get('/test', (req, res) => {
