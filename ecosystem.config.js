bc0a5c6d561ecccdd07a4b7c5281da0d8ce1627d
zhengqizhang <770166635@qq.com>
2019-03-05 15:38:00 +0800

    change name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    name: 'API',
+    name: 'bermi-webhook',
     script: 'index.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
